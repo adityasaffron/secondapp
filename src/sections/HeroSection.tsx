50f7e4b11e09bd31121407e60de59c782c09ed5a
raja <raja.saini@saffrontech.net>
2023-06-19 10:49:04 +0530

    today first commit

--- src/sections/HeroSection.tsx
+++ src/sections/HeroSection.tsx
@@ -2,31 +2,38 @@ import { storefront } from '@site/utilities/storefront';
 import { useState, useEffect } from 'react';
 import Link from 'next/link';
 
+interface Collection {
+  node: {
+    id: string;
+    title: string;
+    handle: string;
+  };
+}
+
 export async function fetchCollections() {
   const { collections } = await storefront.query({
     collections: [
       {
-        first: 12, // Adjust the number as needed
+        first: 12,
       },
       {
         edges: {
           node: {
             id: true,
             title: true,
-            handle: true
+            handle: true,
           },
         },
       },
     ],
   });
 
-  let ccs = collections.edges
-  console.log(ccs, 'collectionscollectionscollectionscollectionscollections');
-  return ccs;
+  const collectionEdges: Collection[] = collections.edges;
+  return collectionEdges;
 }
 
 export function HeroSection() {
-  const [collections, setCollections] = useState([]);
+  const [collections, setCollections] = useState<Collection[]>([]);
 
   useEffect(() => {
     const fetchCollectionsData = async () => {
@@ -42,11 +49,9 @@ export function HeroSection() {
       <h2 className="text-2xl font-bold">Collections</h2>
       <ul className="flex space-x-4">
         {collections.map((collection) => (
-          <li key={collection.node.id }>
-            <Link href={`/collections/${collection.node.handle}` }   className="text-gray-800 hover:text-gray-600">
-             
-                {collection.node.title}
-           
+          <li key={collection.node.id}>
+            <Link href={`/collections/${collection.node.handle}`} className="text-gray-800 hover:text-gray-600">
+              {collection.node.title}
             </Link>
           </li>
         ))}
